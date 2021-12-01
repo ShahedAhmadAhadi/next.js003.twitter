91ed23509d662736e3a0ff53253efe58fe0c1ea5
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-01 22:02:55 +0430

    the pic is not working new pic imported

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import SidebarLink from './SidebarLink'
-import {BellIcon, HashtagIcon, InboxIcon, BookmarkIcon, ClipboardListIcon, UserIcon, DotsCircleHorizontalIcon} from '@heroicons/react/outline'
+import { BellIcon, HashtagIcon, InboxIcon, BookmarkIcon, ClipboardListIcon, UserIcon, DotsCircleHorizontalIcon} from '@heroicons/react/outline'
 import {HomeIcon} from '@heroicons/react/solid'
 
 function Sidebar() {
     return (
         <div className="hidden sm:flex flex-col items-center xl:item-start xl:w-[340px] p-2 fixed h-full">
             <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-                <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+                <Image src="https://spng.subpng.com/20191012/qfr/transparent-animal-icon-bird-icon-social-icon-5da39fde841153.171092641571004382541.jpg" width={30} height={30} />
             </div>
             <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
                 <SidebarLink text="Home" Icon={HomeIcon} active />
