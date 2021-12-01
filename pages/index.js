2530aa39149271321499872445546978fd756f78
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-01 20:44:01 +0430

    components added and the sidebar added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      
-    </div>
-  )
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<Head>
+				<title>Twitter</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<mian>
+				<Sidebar />
+
+			</mian>
+
+		</div>
+	)
 }
