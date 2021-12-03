543514a06b5e058c715c4c4d9cb20cdfa679db68
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-03 17:49:49 +0430

    tweet added to database and posts created for showing the tweets and images container

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,27 @@
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore"
 import { SparklesIcon } from "@heroicons/react/outline"
 import { useSession } from "next-auth/react"
-import Input  from "./Input"
+import { useEffect, useState } from "react"
+import { db } from "../firebase"
+import Input from "./Input"
+import Post from "./Post"
 
 function Feed() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(
+        () =>
+            onSnapshot(
+                query(collection(db, "posts"), orderBy("timestamp", "desc")),
+                (snapshot) => {
+                    setPosts(snapshot.docs);
+                }
+            ),
+        [db]
+    );
+    console.log(posts.map(po => console.log(po.data())))
+
     return (
         <div className="flex-grow text-white border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
             <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
@@ -13,6 +32,9 @@ function Feed() {
             </div>
 
             <Input />
+            <div>
+                {posts.map(post => (<Post key={post.id} id={post.id} post={post.data()} />))}
+            </div>
         </div>
     )
 }
