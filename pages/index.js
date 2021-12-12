6dedcb23e4e8647463d685d0843f900e53c28f80
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-12 12:49:26 +0430

    New Trending component and added to index and following pages

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,56 @@
-import Head from 'next/head'
-import Sidebar from '../components/Sidebar'
-import Feed from '../components/Feed'
-import { getProviders, getSession, useSession } from 'next-auth/react'
-import Login from '../components/Login'
-import Modal from '../components/Modal'
-import Widgets from '../components/Widgets'
+import Head from "next/head";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import Login from "../components/Login";
+import Modal from "../components/Modal";
+import { modalState } from "../atoms/modalAtom";
+import { useRecoilState } from "recoil";
 
-export default function Home({trendingResults, followResults, providers}) {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
 
-	const {data: session} = useSession()
+  if (!session) return <Login providers={providers} />;
 
-	if (!session) {
-		return <Login providers={providers} />
-	}
+  return (
+    <div className="">
+      <Head>
+        <title>Home / Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-	return (
-		<div className="">
-			<Head>
-				<title>Twitter</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
+        <Feed />
+        <Widgets
+          trendingResults={trendingResults}
+          followResults={followResults}
+        />
 
-			<mian className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
-				<Sidebar />
-				<Feed />
-				<Modal />
-				<Widgets />
-			</mian>
-
-		</div>
-	)
+        {isOpen && <Modal />}
+      </main>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-	const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
-		(res) => res.json()
-	);
-	const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
-		(res) => res.json()
-	);
-	const providers = await getProviders();
-	const session = await getSession(context);
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
 
-	return {
-		props: {
-			trendingResults,
-			followResults,
-			providers,
-			session,
-		},
-	};
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
 }
